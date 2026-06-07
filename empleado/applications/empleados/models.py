5d6b08a467dc7421dafbfc8585855245d67fe113
bruno.gnra <bruno.gnra@gmail.com>
2026-06-07 23:02:42 +0200

    cap7_3

--- empleado/applications/empleados/models.py
+++ empleado/applications/empleados/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 from applications.departamento.models import Departamento
 
+# modelo para habilidades de los empleados
+class Habilidades(models.Model):
+    habilidad = models.CharField('Habilidad', max_length=50)
+
+    class Meta:
+        verbose_name = 'Habilidad'
+        verbose_name_plural = 'Habilidades Empleados'
+
+    def __str__(self):
+        return str(self.id) + '-' + self.habilidad
+    
+
 # Create your models here.
 class Empleado(models.Model):
-
 
     JOB_CHOICES = (
         ('0', 'CONTADOR'),
@@ -13,11 +24,17 @@ class Empleado(models.Model):
     )
     
     """ Modelo para tabla empleado"""
-    fist_name = models.CharField('Nombres', max_length=50)
+    first_name = models.CharField('Nombres', max_length=50)
     last_name = models.CharField('Apellidos', max_length=50)
     job = models.CharField('Trabajo', max_length=1, choices=JOB_CHOICES)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
     #image = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
 
+    class Meta:
+        verbose_name = 'Mi Empleado'
+        verbose_name_plural = 'Empleados de la empresa'
+        ordering = ['-first_name', 'last_name']
+        unique_together = ('first_name', 'departamento')
+
     def __str__(self):
-        return str(self.id) + '-' + self.fist_name + '-' + self.last_name
+        return str(self.id) + '-' + self.first_name + '-' + self.last_name
